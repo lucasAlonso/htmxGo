31aa02571a41ec1b97cae1dfc62fe7ba256c6cf4
Lucas <lucasjosealonso@gmail.com>
2023-10-31 21:40:48 -0300

    Project done

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"time"
 )
 
 type Film struct {
@@ -22,6 +23,14 @@ func main() {
 				{Title: "The Thing", Director: "John Carpenter"}}}
 		tmpl.Execute(w, films)
 	}
+	h2 := func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(1 * time.Second)
+		title := r.PostFormValue("title")
+		director := r.PostFormValue("director")
+		tmpl := template.Must(template.ParseFiles("index.html"))
+		tmpl.ExecuteTemplate(w, "film-list-element", Film{Title: title, Director: director})
+	}
 	http.HandleFunc("/", h1)
+	http.HandleFunc("/add-film/", h2)
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
